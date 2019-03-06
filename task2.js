ae9ca3f0f696b5e2a42d494a2c6cdc8790febfe8
Arkady Zelensky <arkasha9606@gmail.com>
2019-03-06 19:02:31 +0200

    HW 2; task 2 done

--- task2.js
+++ task2.js
@@ -1,7 +1,45 @@
 // Задание 2:
+// Функция integers создает бесконечный итератор, который продолжает производить целые числа вечно.
+// Нужно создать функцию take, которая оборачивает данный итератор в другой итератор,
+// останавливающийся по достижении n элементов.
 
 (function () {
 
+  const iter = integers();
 
+  for(let i of take(3, iter)){
+    console.log(i);
+  }
+
+
+  function integers() {
+    let current = 0;
+    
+    return {
+      [Symbol.iterator]: () => ({
+        next() {
+          return {
+            done: false,
+            value: current++,
+          }
+        }
+      })
+    }
+  }
+
+  function take(end, iterator) {    
+    return {
+      [Symbol.iterator]: () => ({
+        next() {
+          const { value } = iterator[Symbol.iterator]().next();
+          
+          return {
+            done: value > end,
+            value
+          }
+        }
+      })
+    }
+  }
 
 })();
