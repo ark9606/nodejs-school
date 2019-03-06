f95e61abab97d381841f805e2de007f4945b015d
Arkady Zelensky <arkasha9606@gmail.com>
2019-03-06 17:30:29 +0200

    HW 2; task 1 done

--- task1.js
+++ task1.js
@@ -1,22 +1,66 @@
 // Задание 1:
-// Реализовать ф-цию sum которая принимает любое кол-во аргументов и возвращает сумму всех аргументов
+// Реализовать добавление группы к юзерам в цикле.
 
 (function () {
-  function sum_es5() {
-    return Array.prototype.reduce.call(arguments, (prevValue, item) =>  prevValue + +item, 0);
-  }
   
-  const sum = (...array) => array.reduce((prevValue, item) => prevValue + +item, 0);
-  
-  
-  const res = [
-    sum(),
-    sum(1),
-    sum(1, 2, 3),
-    sum(1, 2, 3, 4, 5),
+  const users = [
+    {id: 1, name: 'Bob'},
+    {id: 2, name: 'Joe'},
+    {id: 3, name: 'Don', groupId: 1},
+    {id: 4, name: 'Kally'},
+    {name: 'Alex'},
+    {name: 'John'},
   ];
   
-  console.log('Задание 1:', res);
+  const groups = [
+    {id: 1, title: 'First Group'},
+    {id: 2, title: 'Last Group'},
+  ];
+
+
+  const group = groups[1];
+
+  addSelectedGroupToUsers(users, group)
+  .then((res) => console.log(res));
+
+
+  async function addSelectedGroupToUsers(users, group) {
+    const result = [];
+
+    for(let user of users) {
+      let currentUser = {...user};
+
+      if(!('id' in currentUser)) {
+        currentUser = await createUser(currentUser);
+      }
+      if(!('groupId' in currentUser)) {
+        currentUser = await addGroup(currentUser, group.id);
+      }
+
+      result.push(currentUser);
+    }
+
+    return result;    
+  }
+
+
+  function createUser(user) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const randomInt = Math.floor(Math.random() * 1001);
+        resolve({id: randomInt, ...user});
+      }, 500);
+    })
+  }
+
+  function addGroup(user, groupId) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({...user, groupId});
+      }, 500);
+    })
+  }
+
 
 })();
 
